6acf0c5dd460bfd7135f8be2896dc1745c18e125
Victor Aengwo <victoraengwo@gmail.com>
2019-03-14 12:08:30 +0000

    This is the index file.

---
+++ index.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+	<?php include 'includes/head.php'; ?>
+	<title>Hotel Rwanda</title>
+</head>
+<body>
+	<div>
+		<?php include 'includes/navigation.php' ?>
+	</div>
+	<div>
+		<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+			  <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+			  <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+			  <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+			  <div class="carousel-item active">
+				<img src="images/slides/s1.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-caption d-none d-md-block">
+				  <h2 style="">Welcome to <span style="color:#4cff00;">Hotel <span style="color: #fb7416;">Rwanda</span></span></h2>
+				  <p style="color: black"></p>
+				</div>
+			  </div>
+			  <div class="carousel-item">
+				<img src="images/slides/s2.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-caption d-none d-md-block">
+				  <h5>We Value You.</h5>
+				  <p>Welcome to our Hotel where we based our services on quality.</p>
+				</div>
+			  </div>
+			  <div class="carousel-item">
+				<img src="images/slides/s3.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-caption d-none d-md-block">
+				  <h5>Recreational Point</h5>
+				  <p>Welcome to our Hotel where we based our services on quality.</p>
+				</div>
+			  </div>
+			</div>
+		
+			<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+			  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			  <span class="sr-only">Previous</span>
+			</a>
+			
+			<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+			  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			  <span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>
+	<br>
+	<div>
+		<?php include 'includes/footer.php' ?>
+	</div>
+
+</body>
+</html>
