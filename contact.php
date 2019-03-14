0cc7b68f39a4a1a13142dc8f69413a4156052694
Willy Wagwau <wil.wagwau@gmail.com>
2019-03-14 12:43:23 +0300

    Contact and Reservation pages modified for the first time

---
+++ contact.php
@@ -0,0 +1,63 @@
+<html>
+	<head>
+		<?php include 'includes/head.php'; ?>
+		<title>Contact Us</title>
+	</head>
+	<body>
+		<div>
+			<?php include 'includes/navigation.php' ?>
+		</div>
+		
+		<div class="container">
+			<div class="panel panel-info">
+				<div class="panel-body">
+					<br />
+					<b><h6 class="text-info">CONTACT US</h6></b>
+					<hr>
+				
+					<div class="row">
+					
+						<div class="col-md-6"><br>
+							
+						</div>
+						<div class="col-md-6">
+							<center>Write To Us</center>
+							<form>
+								<div class="form-group">
+									<label class="col-sm-2 col-form-label">Email</label>
+									<div class="col-sm-12">
+									  <input type="email" class="form-control" placeholder="email address" required>
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-sm-2 col-form-label">Subject</label>
+									<div class="col-sm-12">
+									  <input type="password" class="form-control" placeholder="enter subject">
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-sm-3 col-form-label">Message</label>
+									<div class="col-sm-12">
+										<textarea class="form-control" rows="4"></textarea>
+									</div>
+								</div>
+							 
+								<div class="form-group">
+									<div class="col-sm-10 col-sm-offset-4">
+									  <button type="submit" class="btn btn-outline-primary"><i class="fa fa-arrow-circle-o-right"></i> send message</button>
+									</div>
+								</div>
+							</form>
+						</div>
+					
+					</div>
+				 
+			</div>
+		</div>
+	</div>
+	
+	<div>
+		<?php include 'includes/footer.php' ?>
+	</div>
+</body>
+</html>
---
+++ reservation.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<?php include 'includes/head.php'; ?>
+	<title>Reservation</title>
+</head>
+<body>
+	<div>
+		<?php include 'includes/navigation.php' ?>
+	</div>
+	
+	<div class="container">
+		<div class="panel panel-info">
+			<div class="panel-body">
+			<br />
+				<strong><h6 class="text-info">MAKE RESERVATION</h6></strong>
+				<hr>
+				
+			</div>
+		</div>
+	</div>
+	<?php include 'includes/footer.php'; ?>
+</body>
+</html>
