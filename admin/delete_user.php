b39ac5b8e40cc7dd3e37644b35b14f9a46a53bae
Willy Wagwau <wil.wagwau@gmail.com>
2019-03-21 16:13:24 +0300

    Implementation of logical delete rather than actual delete

--- admin/delete_user.php
+++ admin/delete_user.php
@@ -1,5 +1,6 @@
+
 <?php
 	require_once 'includes/config.php';
-	mysqli_query($conn, "DELETE FROM `tbl_users` WHERE `user_id` = '$_REQUEST[user_id]'") or die(mysql_error());
+	mysqli_query($conn, "UPDATE `tbl_users` SET `IsDeleted` = b'1', DeletedDate = CURRENT_TIMESTAMP  WHERE `user_id` = '$_REQUEST[user_id]'") or die(mysql_error());
 	header("location:users.php");
 ?>
